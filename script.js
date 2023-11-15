44ed2cdff0f8ddff1341c4b0e285d82ae4c760c5
Tejaswini Khairnar <117677072+Tejaswini1397@users.noreply.github.com>
2023-11-15 09:36:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 //your JS code here. If required.
 function daysOfAYear(year) {
-  // Check if the year is a leap year
-  const isLeapYear = (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
+    const userInput = prompt('Enter a year (between 1 and 9999):');
+const year1 = parseInt(userInput, 10);
 
-  // Return the number of days based on leap year status
-  return isLeapYear ? 366 : 365;
+ // Check if the year is a leap year
+ const isLeapYear = (year1% 4 === 0 && year1% 100 !== 0) || (year1% 400 === 0);
+alert(`Number of days in ${year1}: ${isLeapyear? 366 : 365}`);
+ // Return the number of days based on leap year1status
+ return isLeapYear ? 366 : 365;
+   
 }
+daysOfAYear();
