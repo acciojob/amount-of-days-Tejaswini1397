b296df7a0d3bd201e24abb4b3d3234f11d1f42c2
Tejaswini Khairnar <117677072+Tejaswini1397@users.noreply.github.com>
2023-11-15 09:55:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function daysOfAYear(year) {
   const year1 = parseInt(userInput);
 
  // Check if the year is a leap year
- const isLeapYear = (year1% 4 === 0 && year1% 100 !== 0) || (year1% 400 === 0);
+ const isLeapYear = (year1% 4 === 0 && year1% 100 === 0) || (year1% 400 === 0);
 
  // Return the number of days based on leap year1status
  return isLeapYear ? 366 : 365;
